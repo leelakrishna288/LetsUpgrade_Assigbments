b0998888d258ea293a51d5f1730370f45ccbecf8
leelakrishna288 <65774883+leelakrishna288@users.noreply.github.com>
2021-02-06 11:51:20 +0530

    Day 1 Assignments

---
+++ Day 1 assignments/Assignment 1a.py
@@ -0,0 +1,9 @@
+x,y=map(int,input().split())
+add=x+y
+sub=x-y
+mul=x*y
+div=x/y
+print("Addition:",add)
+print("Subtraction:",sub)
+print("Multiplication:",mul)
+print("Division: {:.2f}".format(div))
