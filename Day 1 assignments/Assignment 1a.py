e3b484563ae9a7a104d2ce2e7410d3266c1cced1
leelakrishna288 <65774883+leelakrishna288@users.noreply.github.com>
2021-02-28 23:08:03 +0530

    Delete Day 1 assignments directory

--- Day 1 assignments/Assignment 1a.py
+++
@@ -1,9 +0,0 @@
-x,y=map(int,input().split())
-add=x+y
-sub=x-y
-mul=x*y
-div=x/y
-print("Addition:",add)
-print("Subtraction:",sub)
-print("Multiplication:",mul)
-print("Division: {:.2f}".format(div))
